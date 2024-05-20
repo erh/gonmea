9e64a948f6540602b212cd3ac175657bf9fb824a
Eliot Horowitz <eliothorowitz@gmail.com>
2024-05-20 17:25:44 -0400

    work with network data

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -7,42 +7,73 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"os"
+	"strings"
 
 	"github.com/erh/gonmea/analyzer"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		panic("need file to parse")
+	err := realMain()
+	if err != nil {
+		panic(err)
 	}
+}
+
+func realMain() error {
+
+	if len(os.Args) != 2 {
+		return fmt.Errorf("need file/path/net to parse")
+	}
+
+	path := os.Args[1]
+	if strings.HasPrefix(path, "net:") {
+		host := path[4:]
+		conn, err := net.Dial("tcp", host)
+		if err != nil {
+			return fmt.Errorf("couldn't connect to host (%s): %w", host, err)
+		}
+		return processData(conn)
+	}
+
 	dataFile, err := os.Open(os.Args[1])
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	return processData(dataFile)
+}
+
+func processData(in io.ReadCloser) error {
+	defer in.Close()
+
 	parser, err := analyzer.NewParser()
 	if err != nil {
-		panic(err)
+		return err
 	}
-	reader := bufio.NewReader(dataFile)
+
+	reader := bufio.NewReader(in)
 	for {
 		line, _, err := reader.ReadLine()
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				return
+				return nil
 			}
-			panic(err)
+			return err
+		}
+		line = []byte(strings.TrimSpace(string(line)))
+		if len(line) == 0 {
+			continue
 		}
 		msg, err := parser.ParseMessage(line)
+		fmt.Printf("hi %v %v %v\n", line, msg, err)
 		if err != nil {
-			if errors.Is(err, io.EOF) {
-				return
-			}
-			panic(err)
+			return err
 		}
 		md, err := json.MarshalIndent(msg, "", "  ")
 		if err != nil {
-			panic(err)
+			return err
 		}
 		fmt.Fprintln(os.Stdout, string(md))
 	}
